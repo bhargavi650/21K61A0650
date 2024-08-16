6278534c9d89d0ccaa373ee5e1ecc3177ce4faba
Bhargavi Ramisetti❤️ <148078250+bhargavi650@users.noreply.github.com>
2024-08-16 13:36:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AverageCalculator from './Calculator';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <AverageCalculator/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
